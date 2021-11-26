041b8ad59a62d9fc70f0ef416ef2cdd292d950d9
Tiep M. H <50709837+TiepMH@users.noreply.github.com>
2021-11-26 22:53:39 +0000

    Add files via upload

---
+++ BPSK__Rayleigh_SISO__MMSE_decoding/BER_in_Rayleigh_fading_SISOsystems_using_MMSE_decoding.m
@@ -0,0 +1,58 @@
+%% Topics:
+% MIMO systems
+% Zero-forcing (ZF) equalization
+% Bit error rate (BER)
+
+%% Assumptions:
+% nRx = 1 transmit antenna, nTx = 1 receive antenna
+% Modulation scheme: BPSK
+% Rayleigh fading channel
+
+%% Simulation
+snrdB_list = (-2:25); % a list of SNRs in dB
+nSim = 10^6;
+BER_sim = zeros(1, length(snrdB_list));
+
+for k = 1:length(snrdB_list)
+    snrdB = snrdB_list(k);
+    snr = 10^(snrdB/10);
+    nErr = 0;
+    for n = 1:nSim
+        %Transmitter
+        s = rand(1, 1) > 0.5; % sequence of 0-bits and 1-bits
+        x = BPSK(s); % signal transmitted by Tx, x is a sequence of -1 and +1
+        %Rayleigh fading channel
+        h = (1/sqrt(2))*(randn(1) + 1i*randn(1));
+        %Receiver
+        n = (1/sqrt(2))*(randn(1) + 1i*randn(1)); % AWGN
+        y = h*x + n/sqrt(snr); % signal received at Rx
+        %% Minimum Mean Squared Error (MMSE)
+        nTx = 1; % the number of transmit antennas
+        nRx = 1; % the number of receive antennas
+        positive_lambda = nTx/snr; % we can choose another positive value for possitive_lambda
+        matrix = h' * (h*h' + positive_lambda*eye(nRx))^-1;
+        xhat = matrix*y; % estimates the transmitted signal
+        shat = real(xhat) > 0; % estimates the transmitted symbol
+        nonzero_indices = find(shat-s); % find() returns a list of indices that are nonzero
+        if ~isempty(nonzero_indices) % if the list is NOT empty, there is a difference between s and shat
+            nErr = nErr + 1; % number of nonzero elements = number of errors
+        end
+    end
+    BER_sim(k) = nErr/nSim;
+end
+
+%% Theory
+snr_list = 10.^(snrdB_list/10);
+BER_theo = 0.5.*(1-sqrt(snr_list./(1+snr_list)));
+
+%% Illustration
+figure
+semilogy(snrdB_list, BER_sim, 'r-o', 'LineWidth', 1);
+hold on
+semilogy(snrdB_list, BER_theo, 'b-', 'LineWidth', 2);
+axis([-2 25, 10^-5 1])
+grid on
+legend('simulation', 'theory');
+xlabel('snr (dB)');
+ylabel('BER');
+title('BPSK mod. & Rayleigh channel & SISO system & MMSE decoding');
